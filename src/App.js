99de842af6c593d45c751d1819c9a01142ebd95c
shahadat2772 <shahadat2772@gmailcom>
2022-06-30 19:30:03 +0600

    button added for adding task

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Navbar from "./pages/Shared/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Completed from "./pages/Completed/Completed";
 import Calendar from "./pages/Calendar/Calendar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
-    <div className="App">
+    <div className="App relative min-h-screen">
       <Navbar></Navbar>
       <div className="max-w-7xl mx-auto">
         <Routes>
@@ -15,6 +17,38 @@ function App() {
           <Route path="/completed" element={<Completed></Completed>}></Route>
           <Route path="/calendar" element={<Calendar></Calendar>}></Route>
         </Routes>
+      </div>
+      <div className="addTaskButton">
+        <button className="button">
+          {/* <FontAwesomeIcon className="plusIcon" icon={faPlus} /> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="plusIcon"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+          {/* <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="plusIcon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg> */}
+          Add
+        </button>
       </div>
     </div>
   );
